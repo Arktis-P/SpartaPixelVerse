007100ad1b14e1d5e0fae4e5683252b04166c200
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-18 12:09:06 +0900

    ⚡ [ add ] add interact button
    add interact button on NPC
    - only activated when player is nearby (in npc's trigger collider)

--- Assets/Scripts/Entity/NPCController.cs
+++ Assets/Scripts/Entity/NPCController.cs
@@ -7,6 +7,16 @@ public class NPCController : BaseController
 {
     private float timer = 0f;
     private float timeInterval = 2f;
+    // for interact buttons
+    public GameObject interactButton;
+    private bool isInside = false;  // check if player is in its collider area
+
+    protected override void Start()
+    {
+        // initialize interact button
+        if (interactButton == null) return;
+        interactButton.SetActive(false);
+    }
 
     // set random lookDirection every 2 sec for npc flip
     protected override void Update()
@@ -29,5 +39,17 @@ public class NPCController : BaseController
             Random.Range(-5f,5f), Random.Range(-3f,3f)
             );
         return direction = direction.normalized;
+    }
+
+    // show button when player is in npc's collider area
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isInside = true;
+        interactButton.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInside = false;
+        interactButton.SetActive(false);
     }
 }
