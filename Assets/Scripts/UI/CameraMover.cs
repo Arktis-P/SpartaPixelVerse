969e434165ab7e64decfebb93a0620c23033ea24
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-18 19:04:03 +0900

    ⚡ [ add ] obstacle looping (script)

--- Assets/Scripts/UI/CameraMover.cs
+++ Assets/Scripts/UI/CameraMover.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraMover : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class CameraMover : MonoBehaviour
     private void Start()
     {
         if (target == null) return;  // process exception
+        // if on Flappy game screen, get offsetX (distance from camera to player)
+        if (SceneManager.GetActiveScene().buildIndex == 1) offsetX = transform.position.x - target.position.x;
     }
 
     private void Update()
@@ -17,7 +20,8 @@ public class CameraMover : MonoBehaviour
         if (target == null) return;  // process exception
         // move camera as player obj moves
         Vector3 pos = transform.position;
-        pos.x = target.position.x; pos.y = target.position.y;
+        pos.x = target.position.x;
+        if (SceneManager.GetActiveScene().buildIndex == 0) pos.y = target.position.y;
         transform.position = pos;
     }
 }
