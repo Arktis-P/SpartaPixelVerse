2811cd90646a1d86b1efd0897574d31f07a81b0c
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-18 09:33:34 +0900

    ✨ [ feat ] add player moving func
    add player moving func
    - used player input
    - connected controllers
    - connected animations

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private Transform weaponPivot;
+    // movement directions => arrow keys or wasd
+    protected Vector2 moveDirection = Vector2.zero;
+    public Vector2 MoveDirection { get { return moveDirection; } }
+    // looking direction => change according to mouse position (for player)
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+    // handlers for entities
+    protected AnimationHandler animationHandler;
+    protected StatHandler statHandler;
+
+    protected virtual void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        animationHandler = GetComponent<AnimationHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Start()
     {
-        
+
+    }
+
+    protected virtual void Update()
+    {
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()  // update for fixed frame rate
+    {
+        Move(moveDirection);
+    }
+
+    private void Move(Vector2 direction)  // move at certain speed in the direction
+    {
+        direction = direction * 5f;
+
+        _rigidbody.velocity = direction;
+        animationHandler.Move(direction);
+    }
+    private void Rotate(Vector2 direction)  // rotat entity at the direction of its looking
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterRenderer.flipX = isLeft;  // convert status of flipX if isLeft is true
     }
 }
--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : BaseController
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera camera;
+
+    protected override void Start()
     {
-        
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    // events with on input
+    void OnMove(InputValue inputValue)
     {
-        
+        moveDirection = inputValue.Get<Vector2>();
+        moveDirection = moveDirection.normalized;
+    }
+    void OnLook(InputValue inputValue)
+    {
+        Vector2 mousePos = inputValue.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePos);
+        lookDirection = worldPos - (Vector2)transform.position;
+
+        if (lookDirection.magnitude < .9f) lookDirection = Vector2.zero;
+        else lookDirection = lookDirection.normalized;
     }
 }
