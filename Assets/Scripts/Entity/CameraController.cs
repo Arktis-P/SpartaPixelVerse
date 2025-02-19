7616c69dd62c986359905580c96d007c5c21eb2b
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-19 09:12:25 +0900

    🔧 [ fix ] flappy camera position
    fix flappy camera's position slightly left

--- Assets/Scripts/Entity/CameraController.cs
+++ Assets/Scripts/Entity/CameraController.cs
@@ -13,6 +13,7 @@ public class CameraController : MonoBehaviour
         if (target == null) return;  // process exception
         // if on Flappy game screen, get offsetX (distance from camera to player)
         if (SceneManager.GetActiveScene().buildIndex == 1) offsetX = transform.position.x - target.position.x;
+        else offsetX = 0f;
     }
 
     private void Update()
@@ -20,7 +21,7 @@ public class CameraController : MonoBehaviour
         if (target == null) return;  // process exception
         // move camera as player obj moves
         Vector3 pos = transform.position;
-        pos.x = target.position.x;
+        pos.x = target.position.x + offsetX;
         if (SceneManager.GetActiveScene().buildIndex == 0) pos.y = target.position.y;
         transform.position = pos;
     }
