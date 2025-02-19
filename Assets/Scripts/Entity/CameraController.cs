f252fbacf3ec8e8618c9823379046662f9df611b
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-19 21:20:55 +0900

    ⚡ [ add ] lock camera on map
    add func to lock camera on map
    - now, camera cannot go outside of map anymore

--- Assets/Scripts/Entity/CameraController.cs
+++ Assets/Scripts/Entity/CameraController.cs
@@ -8,21 +8,40 @@ public class CameraController : MonoBehaviour
     public Transform target;
     float offsetX;
 
+    float limitX = 4.19f;
+    float limitY = 3.25f;
+
     private void Start()
     {
         if (target == null) return;  // process exception
         // if on Flappy game screen, get offsetX (distance from camera to player)
-        if (SceneManager.GetActiveScene().buildIndex == 1) offsetX = transform.position.x - target.position.x;
+        if (SceneManager.GetActiveScene().buildIndex == (int)Scenes.MainScene) offsetX = transform.position.x - target.position.x;
         else offsetX = 0f;
     }
 
     private void Update()
     {
         if (target == null) return;  // process exception
-        // move camera as player obj moves
+        // hold camera in map area
         Vector3 pos = transform.position;
-        pos.x = target.position.x + offsetX;
-        if (SceneManager.GetActiveScene().buildIndex == 0) pos.y = target.position.y;
+        // lock camera on main scene
+        if (SceneManager.GetActiveScene().buildIndex == (int)Scenes.MainScene)
+        {
+            // set basic location
+            pos.x = target.position.x;
+            pos.y = target.position.y;
+            // lock location
+            if (pos.x >= limitX) pos.x = limitX;
+            if (pos.x <= -limitX) pos.x = -limitX;
+            if (pos.y >= limitY) pos.y = limitY;
+            if (pos.y <= -limitY) pos.y = -limitY;
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == (int)Scenes.FlappyGameScene)
+        {
+            pos.x = target.position.x + offsetX;
+            pos.y = target.position.y;
+        }
+
         transform.position = pos;
     }
 }
